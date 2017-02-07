a241321edff770121309a8475407a707469fb02a
mklau <matthewklau@fas.harvard.edu>
2017-02-07 18:23:38 -0500

    adds initial analyses and methods

--- src/ApG.R
+++ src/ApG.R
@@ -6,7 +6,25 @@
 ## Basic analysis of genomic similarity across species
 ## Basic analysis of target genes
 
-library(bioconductor)
+source("https://bioconductor.org/biocLite.R")
+library(magrittr)
+
+## sample information
+
+info <- gdata::read.xls('/Volumes/ellison_lab/ap_genomes/SSF-1728_KeyforCollaborator.xlsx',sheet = 2)
+
+fasta <- (lapply(dir('/Volumes/ellison_lab/ap_genomes/heads/',
+                    'fasta',full = TRUE),readLines) %>% #read fasta files from Odyssey
+              lapply(FUN = paste0,collapse = '') %>% #paste lines together
+                  lapply(FUN = strsplit,split = '>') %>% #split based on contig signifier
+                      lapply(FUN = function(x) x[[1]][c(-1,-length(x))])
+          )
+
+## 0. genome quality (from the gaemer results)
+
+## GC content
+## percent contaminants
+## 
 
 ## 1. Heat stress response genes @Stanton-Geddes2016
 
