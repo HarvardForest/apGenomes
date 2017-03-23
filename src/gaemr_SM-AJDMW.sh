1397070c932fc2a585434a8cb0ae088cf11f7f05
MKLau <matthewklau@fas.harvard.edu>
2017-03-22 21:33:15 -0400

    adjusting memory and core usage

--- src/gaemr_SM-AJDMW.sh
+++ src/gaemr_SM-AJDMW.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -n 1                    # Number of cores
+#SBATCH -n 10                    # Number of cores
 #SBATCH -N 1                    # Ensure that all cores are on one machine
 #SBATCH -t 0-01:00              # Runtime in D-HH:MM
-#SBATCH -p serial_requeue       # Partition to submit to
-#SBATCH --mem=1000               # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -p general       # Partition to submit to
+#SBATCH --mem=10000               # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o gaemr_%j.out      # File to which STDERR will be written
 #SBATCH -e gaemr_%j.err      # File to which STDERR will be written
 #SBATCH --mail-type=ALL         # Type of email notification- BEGIN,END,FAIL,ALL
@@ -11,4 +11,4 @@
 
 cd /n/home10/mklau/storage/ap_genomes/SM-AJDMW
 module load GAEMR/1.0.1-fasrc03
-GAEMR.py -c filtered.contigs.fasta --scaffolds=filtered.scaffolds.fasta --agp=filtered.agp --force
+GAEMR.py --threads=10 -c filtered.contigs.fasta --scaffolds=filtered.scaffolds.fasta --agp=filtered.agp --force
--- src/gaemr_SM-AZXXO.sh
+++ src/gaemr_SM-AZXXO.sh
@@ -2,8 +2,8 @@
 #SBATCH -n 1                    # Number of cores
 #SBATCH -N 1                    # Ensure that all cores are on one machine
 #SBATCH -t 0-01:00              # Runtime in D-HH:MM
-#SBATCH -p serial_requeue       # Partition to submit to
-#SBATCH --mem=1000               # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -p general       # Partition to submit to
+#SBATCH --mem=10000               # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o gaemr_%j.out      # File to which STDERR will be written
 #SBATCH -e gaemr_%j.err      # File to which STDERR will be written
 #SBATCH --mail-type=ALL         # Type of email notification- BEGIN,END,FAIL,ALL
