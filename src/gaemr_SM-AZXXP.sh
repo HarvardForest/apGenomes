4b1789ab0899ab56bd70bf884f4e5d1f4a3bb321
MKLau <matthewklau@fas.harvard.edu>
2017-03-25 13:46:39 -0400

    adds blast back and increases run time to 10hrs

--- src/gaemr_SM-AZXXP.sh
+++ src/gaemr_SM-AZXXP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -n 8                    # Number of cores
 #SBATCH -N 1                    # Ensure that all cores are on one machine
-#SBATCH -t 0-03:00              # Runtime in D-HH:MM
+#SBATCH -t 0-10:00              # Runtime in D-HH:MM
 #SBATCH -p general       # Partition to submit to
 #SBATCH --mem-per-cpu=10000               # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o azxxp_%j.out      # File to which STDOUT will be written
@@ -11,4 +11,4 @@
 
 cd /n/home10/mklau/storage/ap_genomes/SM-AZXXP
 module load GAEMR/1.0.1-fasrc03
-GAEMR.py --threads=8 -c filtered.contigs.fasta --scaffolds=filtered.scaffolds.fasta --assembler=allPaths --agp=filtered.agp --force --no_blast=true
+GAEMR.py --threads=8 -c filtered.contigs.fasta --scaffolds=filtered.scaffolds.fasta --assembler=allPaths --agp=filtered.agp --force 
