eeb423b4731e5d1d90ae73237a2b075bdb63e341
MKLau <matthewklau@fas.harvard.edu>
2017-03-25 13:13:44 -0400

    removes blast adds assembly option adjusts cores

--- src/gaemr_SM-AZXXO.sh
+++ src/gaemr_SM-AZXXO.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH -n 1                    # Number of cores
+#SBATCH -n 8                    # Number of cores
 #SBATCH -N 1                    # Ensure that all cores are on one machine
-#SBATCH -t 0-01:00              # Runtime in D-HH:MM
+#SBATCH -t 0-03:00              # Runtime in D-HH:MM
 #SBATCH -p general       # Partition to submit to
-#SBATCH --mem=10000               # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o gaemr_%j.out      # File to which STDERR will be written
-#SBATCH -e gaemr_%j.err      # File to which STDERR will be written
+#SBATCH --mem-per-cpu=10000               # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o azxxo_%j.out      # File to which STDOUT will be written
+#SBATCH -e azxxo_%j.err      # File to which STDERR will be written
 #SBATCH --mail-type=ALL         # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=matthewklau@fas.harvard.edu # Email to which notifications will be sent
 
 cd /n/home10/mklau/storage/ap_genomes/SM-AZXXO
 module load GAEMR/1.0.1-fasrc03
-GAEMR.py -c filtered.contigs.fasta --scaffolds=filtered.scaffolds.fasta --agp=filtered.agp --force
+GAEMR.py --threads=8 -c filtered.contigs.fasta --scaffolds=filtered.scaffolds.fasta --assembler=allPaths --agp=filtered.agp --force --no_blast=true
