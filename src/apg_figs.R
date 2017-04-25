ea0058d5b420ca96d56f0747ea9c56f2bd332735
mklau <matthewklau@fas.harvard.edu>
2017-04-24 23:11:47 -0400

    adds size and gc comps

--- src/apg_figs.R
+++ src/apg_figs.R
@@ -1,5 +1,7 @@
-### Collect gaemr data for making tables and figures
+### Load other ant genome information
+source('genome_size.R')
 
+### Collect gaemr data for making tables and figures
 apgs <- dir('~/storage/ap_genomes',full = TRUE)
 apgs <- apgs[grepl('SM-',apgs)]
 
@@ -23,7 +25,6 @@ for (i in 1:length(apgs)){
                       )
 }
 
-
 stats <- lapply(df,function(x) x[[1]])
 stats <- lapply(stats,function(x) x[-1:-2,])
 stats <- lapply(stats,sub, pattern = " ",replacement = "")
@@ -45,4 +46,19 @@ stats.ord <- c("Contigs","MaxContig","MeanContig","ContigN50","ContigN90","Total
 all(colnames(stats)[match(stats.ord,colnames(stats))] == stats.ord)
 stats <- stats[,match(stats.ord,colnames(stats))]
 
+### Make the table
 print(xtable::xtable(stats),type = "latex",file = "../docs/manuscript/assembly_stats.tex")
+
+### Inter-species comparisons
+size.xlim <- range(as.numeric(c((stats[,'TotalScaffoldLength'] / (1000000)),ncbi.ant[,'Size (Mb)'])))
+size.xlim <- c(floor(size.xlim[1]),ceiling(size.xlim[2]))
+gc.xlim <- range(as.numeric(c(stats[,'AssemblyGC'],ncbi.ant[,'GC%'])))
+gc.xlim <- c(floor(gc.xlim[1]),ceiling(gc.xlim[2]))
+
+par(mfrow = c(1,2))
+plot(density(as.numeric(ncbi.ant[,'Size (Mb)'])),main = '',xlab = 'Size (Mb)',xlim = size.xlim)
+abline(v = stats[,'TotalScaffoldLength'] / (1000000),col = 'grey')
+plot(density(as.numeric(ncbi.ant[,'GC%'])),main = '',xlab = 'GC%',xlim = gc.xlim)
+abline(v = stats[,'AssemblyGC'],col = 'grey')
+
+
--- src/getGaemr.R
+++ src/getGaemr.R
@@ -1,7 +1,8 @@
 ### Compile gaemr output
 
-apg.dir <- "~/stoarage/ap_genomes"
+apg.dir <- tail(commandArgs(trailingOnly = TRUE),1)
 
+apg.dir <- '/Volumes/ellison_lab/ap_genomes'
 
 apg.files <- dir(apg.dir)
 apg.samples <- apg.files[grepl("SM",apg.files)]
@@ -9,6 +10,7 @@ apg.files <- paste0(apg.dir,"/",apg.samples,"/gaemr")
 apg.stats <- lapply(paste0(apg.files,"/","table/",
                            "assembly.basic_assembly_stats.table.txt"),
                     readLines)
+### gep CpG content ????
 apg.stats <- lapply(apg.stats,function(x) x[-1:-3])
 apg.stats <- lapply(apg.stats,strsplit,split="\\|")
 apg.stats <- lapply(apg.stats,function(x) do.call(rbind,x))
@@ -26,3 +28,4 @@ apg.stats <- data.frame(apg.stats)
 apg.stats <- apply(apg.stats,2,as.numeric)
 rownames(apg.stats) <- apg.samples
 write.csv(apg.stats,"../docs/filtered_gaemr.csv")
+
